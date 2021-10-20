7ae3d2c1e25f1444690e3e752aa7fa6c8fc63081
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-10-20 05:00:10 +0000

    [add2]5-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root to: "homes#top"
+  resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only:[:show, :edit, :update]
+  end
--- db/migrate/20211018123228_add_column_user.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnUser < ActiveRecord::Migration[5.2]
-  def change
-    add_colum :introduction, :text
-
-    add_colum :profile_image_id, :integer
-  end
-end
